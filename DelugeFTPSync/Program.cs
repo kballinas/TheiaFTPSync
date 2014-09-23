68b901294979fe2b7723919f44cda9ffcacb86a8
kballinas <kevinballinas@yahoo.com.mx>
2014-09-23 15:47:43 -0500

    initial code commit
    Basic connect, read directory, and log last run time

---
+++ DelugeFTPSync/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheiaFTPSync.Core;
+
+namespace TheiaFTPSync
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ftpSync = new FTPSync();
+            ftpSync.RunFTPSync();
+            Console.ReadKey();
+        }
+    }
+}
