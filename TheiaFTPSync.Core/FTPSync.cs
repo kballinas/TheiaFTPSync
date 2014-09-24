456fd21d7972e7e60a3abbed46a7c1c02c0a5c36
kballinas <kevinballinas@yahoo.com.mx>
2014-09-24 16:25:53 -0500

    Untested sync
    good starting point to expand for individual needs

--- TheiaFTPSync.Core/FTPSync.cs
+++ TheiaFTPSync.Core/FTPSync.cs
@@ -1,4 +1,5 @@
 ﻿using Renci.SshNet;
+using Renci.SshNet.Sftp;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -15,17 +16,45 @@ namespace TheiaFTPSync.Core
         {
             var now = DateTime.UtcNow.ToLocalTime();
             var lastRun = ReadLastRun();
-            using (SftpClient ftp = new SftpClient(ConfigurationManager.AppSettings["FTP_URL"], 
-                ConfigurationManager.AppSettings["FTP_Username"], 
+            using (SftpClient ftp = new SftpClient(ConfigurationManager.AppSettings["FTP_URL"],
+                ConfigurationManager.AppSettings["FTP_Username"],
                 ConfigurationManager.AppSettings["FTP_Password"]))
             {
                 var ftp_directory = ConfigurationManager.AppSettings["FTP_Directory"];
                 ftp.Connect();
-                var files = ftp.ListDirectory(ftp_directory);
                 ftp_directory.Split('/').ToList().ForEach(f => ftp.ChangeDirectory(f));
+                var files = ftp.ListDirectory(".");
 
-                files=files.Where(f => f.Attributes.LastWriteTime > lastRun && f.Attributes.LastWriteTime < now);
-                //WriteLastRan(now.ToString()); //uncommentn after done testing
+                files = files.Where(f => !f.Name.Equals(".") && !f.Name.Equals("..")).Where(f => f.LastWriteTime > lastRun && f.LastWriteTime < now).Where(f =>
+                {
+                    var actualTime = f.ActualLastWriteTime(ftp, ftp_directory);
+                    return
+                    actualTime > lastRun &&
+                    actualTime < now;
+                }).ToList();
+                DownloadFiles(files, ftp, ftp_directory);
+                WriteLastRan(now.ToString()); //uncommentn after done testing
+            }
+        }
+
+        private void DownloadFiles(IEnumerable<SftpFile> files, SftpClient ftp, string root)
+        {
+            foreach (var file in files)
+            {
+                if (file.IsDirectory)
+                {
+                    Directory.CreateDirectory(string.Format("{0}\\{1}", ConfigurationManager.AppSettings["Local_Directory"], file.FullName.Substring(file.FullName.IndexOf(root, 0) + root.Length)));
+                    var children = ftp.ListDirectory(string.Format("./{0}", file.FullName.Substring(file.FullName.IndexOf(root, 0) + root.Length))).Where(w => !w.Name.Equals(".") && !w.Name.Equals(".."));
+                    DownloadFiles(children, ftp, root);
+                }
+                else
+                {
+                    using (Stream fileStream = File.OpenWrite(string.Format("{0}\\{1}", ConfigurationManager.AppSettings["Local_Directory"], file.FullName.Substring(file.FullName.IndexOf(root, 0) + root.Length))))
+                    {
+                        ftp.DownloadFile(file.Name, fileStream);
+                        fileStream.Flush();
+                    }
+                }
             }
         }
 
@@ -36,7 +65,7 @@ namespace TheiaFTPSync.Core
             {
                 using (var lastRanReader = new StreamReader(lastRanStream))
                 {
-                    rtn=DateTime.Parse(lastRanReader.ReadToEnd());
+                    rtn = DateTime.Parse(lastRanReader.ReadToEnd());
                 }
             }
             return rtn.Value;
@@ -49,9 +78,30 @@ namespace TheiaFTPSync.Core
                 using (var lastRanWriter = new StreamWriter(lastRanStream))
                 {
                     lastRanWriter.WriteLine(text);
-                    lastRanWriter.Flush();
                 }
             }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static DateTime ActualLastWriteTime(this SftpFile file, SftpClient ftp, string root)
+        {
+            var rtn = file.Attributes.LastWriteTime;
+
+            if (file.Attributes.IsDirectory)
+            {
+                var children = ftp.ListDirectory(string.Format("./{0}", file.FullName.Substring(file.FullName.IndexOf(root, 0) + root.Length)))
+                    .Where(w => !w.Name.Equals(".") && !w.Name.Equals(".."));
+                var lastWrittenFile = children
+                    .OrderBy(o => o.ActualLastWriteTime(ftp, root)).LastOrDefault();
+                if (lastWrittenFile != null)
+                {
+                    rtn = lastWrittenFile.ActualLastWriteTime(ftp, root);
+                }
+            }
+
+            return rtn;
         }
     }
 }
