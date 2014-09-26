029dc275cee2e98720d3954e343576fd72b00b9a
kballinas <kevinballinas@yahoo.com.mx>
2014-09-26 16:38:14 -0500

    Added windows service project
    this meant to run as a service not as a console application.

---
+++ TheiaFTPSync.Service/TheiaSyncSerive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using TheiaFTPSync.Core;
+
+namespace TheiaFTPSync.Service
+{
+    public partial class TheiaSyncSerive : ServiceBase
+    {
+        public TheiaSyncSerive()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var ftpSync = new FTPSync();
+            ftpSync.RunFTPSync();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
